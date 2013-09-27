b4cd0dd0dbca91e51f2bc14d2d7515777f03f94a
Ale Feltes <awi@alefq.com>
2013-09-26 22:03:02 -0400

    Clase sobre JPA

---
+++ src/main/java/com/alefq/java3/auditoria_chat/business/RolBC.java
@@ -0,0 +1,26 @@
+package com.alefq.java3.auditoria_chat.business;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.alefq.java3.auditoria_chat.domain.Rol;
+import com.alefq.java3.auditoria_chat.persistence.RolDAO;
+
+import org.ticpy.tekoporu.template.DelegateCrud;
+
+public class RolBC extends DelegateCrud<Rol, Long, RolDAO> {
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private RolDAO rolDAO;
+	
+	public List<Rol> findPage(int pageSize, int first, String sortField, boolean sortOrderAsc){
+		return rolDAO.findPage(pageSize,first, sortField, sortOrderAsc);
+	}
+	
+	public int count() {
+		return rolDAO.count();
+	}
+	
+}
---
+++ src/main/java/com/alefq/java3/auditoria_chat/domain/ConexionAudit.java
@@ -0,0 +1,64 @@
+package com.alefq.java3.auditoria_chat.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the conexion_audit database table.
+ * 
+ */
+@Entity
+@Table(name="conexion_audit")
+public class ConexionAudit implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="CONEXION_AUDIT_CONEXIONAUDITID_GENERATOR", sequenceName="CONEXION_AUDIT_CONEXION_AUDIT_ID_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="CONEXION_AUDIT_CONEXIONAUDITID_GENERATOR")
+	@Column(name="conexion_audit_id")
+	private Long conexionAuditId;
+
+	private Timestamp fecha;
+
+	private String nickname;
+
+	private String tipo;
+
+    public ConexionAudit() {
+    }
+
+	public Long getConexionAuditId() {
+		return this.conexionAuditId;
+	}
+
+	public void setConexionAuditId(Long conexionAuditId) {
+		this.conexionAuditId = conexionAuditId;
+	}
+
+	public Timestamp getFecha() {
+		return this.fecha;
+	}
+
+	public void setFecha(Timestamp fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getNickname() {
+		return this.nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
